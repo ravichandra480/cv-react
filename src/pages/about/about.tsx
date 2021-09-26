07d0811f06c7bbe119b12432e642cd610480856c
Ravi Ravulapati <ravi.ravulapati@featurespace.co.uk>
2021-09-26 15:52:07 +0100

    About me update

--- src/pages/about/about.tsx
+++ src/pages/about/about.tsx
@@ -8,13 +8,11 @@ function About() {
 	return (
 		<main className="about">
 			<img src={me} className="about__me" alt="Ravi" />
-			<ul>
-				<li>Working as Senior Engineer 2 with <a href="https://www.featurespace.com/" target="_blank" rel="noreferrer">Featurespace</a>.</li>
-				<li>Over 10 years of developing web and mobile applications using HTML5, JavaScript, TypeScript, JavaScript libraries (Angular, Jquery), J2EE, NodeJS.</li>
-				<li>Over 9 years of experience in CSS3, CSS Pre-processors (LESS, SCSS) to customize the look and feel of both web and mobile applications.</li>
-				<li>Over 8 years of experience in developing cross browser compatible web applications (Gracefully degrading the HTML5 features using Modernizr java script library).</li>
-				<li>Over 6 years of experience in TDD, Test automation and DevOps.</li>
-			</ul>
+			<div>
+				<p className="about__name">Ravi Chandra Ravulapati (He/Him)</p>
+				<p className="about__job">Senior Engineer 2 at <a href="https://www.featurespace.com/" target="_blank" rel="noreferrer">Featurespace</a> <br />Senior Web Developer | Front End Architect | JavaScript | TypeScript | Angular<br /></p>
+				<p className="about__tags">Talks about #javascript, #websecurity, #accessability, #webdevelopment, and #frontenddevelopment</p>
+			</div>
 		</main>
 	)
 }
