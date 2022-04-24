6928cf18d0bb30950916d029de0410751de7b0d1
Ravi Ravulapati <ravi.ravulapati@featurespace.co.uk>
2022-04-24 21:05:33 +0100

    Updated about info

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,7 +1,7 @@
 function Footer() {
 
 	return (
-		<footer>©2021</footer>
+		<footer>©2022</footer>
 	);
 }
 
--- src/pages/about/about.tsx
+++ src/pages/about/about.tsx
@@ -10,7 +10,7 @@ function About() {
 			<img src={me} className="about__me" alt="Ravi" />
 			<div>
 				<p className="about__name">Ravi Chandra Ravulapati (He/Him)</p>
-				<p className="about__job">Senior Engineer 2 at <a href="https://www.featurespace.com/" target="_blank" rel="noreferrer">Featurespace</a> <br />Senior Web Developer | Front End Architect | JavaScript | TypeScript | Angular<br /></p>
+				<p className="about__job">Senior Engineer 2 at <a href="https://www.featurespace.com/" target="_blank" rel="noreferrer">Featurespace</a> <br />Senior Web Developer | Front End Architect | JavaScript | TypeScript | Angular | Elm<br /></p>
 				<p className="about__tags">Talks about #javascript, #websecurity, #accessability, #webdevelopment, and #frontenddevelopment</p>
 			</div>
 		</main>
